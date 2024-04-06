b89b4383cc780967ece4939801125f5961f3f53b
Jayant Rohila <jayant@binmile.com>
2024-04-06 19:00:25 +0530

    Cat page

--- app/(products)/[cat]/[prod]/[slug]/page.tsx
+++ app/(products)/[cat]/[prod]/[slug]/page.tsx
@@ -1,7 +1,11 @@
-import React from "react";
+import ProductBySlug from "@/components/products/product-page";
+import { getProductBySlug } from "@/server/api-sanity";
+import { IParams, IProduct } from "@/types/interface";
 
-const Page = () => {
-  return <div>Page</div>;
-};
+export const revalidate = 10;
+
+const Page = async ({ params }: IParams) => (
+  <ProductBySlug data={(await getProductBySlug(params.slug)) as IProduct} />
+);
 
 export default Page;
--- app/(products)/[cat]/[prod]/page.tsx
+++ app/(products)/[cat]/[prod]/page.tsx
@@ -1,7 +1,13 @@
-import React from "react";
 
-const Page = () => {
-  return <div>Page</div>;
-};
+import ProductPage from "@/components/products/products-page";
+import { getAllProduct } from "@/server/api-sanity";
+
+export const revalidate = 10;
+
+const Page = async ({
+  searchParams,
+}: {
+  searchParams: { [key: string]: string | string[] | undefined };
+}) => <ProductPage data={await getAllProduct({ searchParams })} />;
 
 export default Page;
